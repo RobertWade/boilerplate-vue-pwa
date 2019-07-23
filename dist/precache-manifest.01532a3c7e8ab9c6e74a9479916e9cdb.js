f29053ca05f1a7aa4626b171a5b4d95c7cdaa31f
Robert Wade <robertwade@Roberts-MacBook-Pro.local>
2019-07-23 07:54:59 +0200

    added dist

---
+++ dist/precache-manifest.01532a3c7e8ab9c6e74a9479916e9cdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "43647b5b83a54368f688",
+    "url": "/js/chunk-vendors.d207454b.js"
+  },
+  {
+    "revision": "e23c6a8b5101a5be4f5a",
+    "url": "/js/app.037e368b.js"
+  },
+  {
+    "revision": "960c040b3d6f9d7bb1d5",
+    "url": "/js/about.df5631a8.js"
+  },
+  {
+    "revision": "cfb21dfa2676ad421746a2340e64090c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e23c6a8b5101a5be4f5a",
+    "url": "/css/app.f75df495.css"
+  }
+];
