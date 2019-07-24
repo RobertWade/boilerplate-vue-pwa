ec86edfa4a33832676cfe377fc69ba484862b60f
Robert Wade <robertwade@Roberts-MacBook-Pro.local>
2019-07-24 17:09:00 +0200

    renaming

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <p>This is a boilerplate PWA. I added some basic functionality. Like routing with fade transition, toggle between dark and light mode, testing, basic components and other cool stuff.</p>
+    <p>This is a boilerplate PWA. I added some basic functionality. Like routing with fade transition, toggle between dark and light mode, testing, components and other cool stuff.</p>
   </div>
 </template>
 
